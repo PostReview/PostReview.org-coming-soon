6c86c7a34518f80b483484cac0560179144f5d51
Nami Sunami <naoyuki.sunami@gmail.com>
2022-05-18 08:50:20 -0400

    Install tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ["{pages,app}/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: ['"Fira Sans"', "sans-serif"],
+    },
+  },
+  plugins: [],
+}
